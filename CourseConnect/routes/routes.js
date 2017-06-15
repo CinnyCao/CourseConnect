b66be4927a65399d1685d9da58cf910a3b2734d5
Samuel Gao <samuel.gao3@gmail.com>
2017-06-15 01:16:48 -0400

    Added setup for final project. Support DB setup, templates, routes, controller, API. Currently set every request to PageNotFound. Will add more in the next commit

---
+++ CourseConnect/routes/routes.js
@@ -0,0 +1,54 @@
+// Route handler of App server.
+"use strict;"
+
+
+/*
+ * ================ Set up ================
+ */
+var mysql = require('mysql'),      // Nodejs driver for MySQL
+    util  = require('util'),       // Util lib facilitates console.log()
+    async = require('async'),      // Async lib helps asynchronized execution
+    config = require('./../config.js');  // App's local config - port#, etc
+
+
+/*
+ * ================ Create Pool for SQL DB ================
+ */
+var pool = mysql.createPool({
+  host     : config.dbhost,
+  database : config.dbname,
+  user     : config.dbuser,
+  password : config.dbpwd,
+  connectionLimit : 10
+});
+
+
+/**
+ * Get one connection from DB pool
+ */
+exports.requestDbConnection = function(callback) {
+
+  // Connect to DB
+  pool.getConnection(function (connErr, connection) {
+    if (connErr) {
+      connection.destroy();
+      console.log("ERROR: Failed to connect to database. " + connErr);
+      resBody["error"] = "Failed to connect to database."
+      res.status(502).send(resBody);
+    } else {
+      console.log("Connected to database.");
+      callback(connection);
+    }});
+};
+
+/*
+ *  ================ App routes (API) ================
+ */
+
+/*
+ * Heartbeat test
+ */
+exports.api = function(req, res) {
+  res.status(200).send('<h3>Course Connect API is running...</h3>');
+};
+
---
+++ CourseConnect/server.js
@@ -0,0 +1,55 @@
+"use strict";
+
+/* App server of Course Connect */
+
+/*
+ * ==== Set up ========
+ */
+var http = require('http'),                 // Http interface
+    express = require('express'),           // Express web framework
+    compression = require('compression'),   // Compression middleware
+    morgan = require('morgan'),             // HTTP request logger middleware
+    errorhandler = require('errorhandler'), // Dev-only error handler middleware
+    bodyParser = require('body-parser'),    // Parse data body in post request
+    fs = require('fs'),                     // File system
+    config = require('./config.js'),        // App's local config - port#, etc
+    portal = require('./routes/routes.js'); // Routes handlers
+
+
+/*
+ * ==== Create Express app server ========
+ */
+ var app = express();
+
+// Configurations
+
+// use port value in local config file
+app.set('port', config.port);
+
+// change param value to control level of logging
+app.use(morgan('dev'));   // 'default', 'short', 'tiny', 'dev'
+
+// use compression (gzip) to reduce size of HTTP responses
+app.use(compression());
+
+// return error details to client - use only during development
+app.use(errorhandler({ dumpExceptions:true, showStack:true })); 
+
+// parse application/json 
+app.use(bodyParser.json());
+
+
+/*
+ * App routes (API) - route-handlers implemented in routes/*
+ */
+app.get('/api', portal.api);
+
+// location of app's static content
+app.use(express.static(__dirname + "/app"));
+
+// ==== Start HTTP server ========
+http.createServer(app).listen(app.get('port'), function () {
+    console.log("Express server listening on port %d in %s mode", 
+      app.get('port'), config.env 
+    );
+});
