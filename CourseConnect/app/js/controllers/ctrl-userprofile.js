61c8b2ebf99a66467e69b8efa66063037486d8ba
Samuel Gao <samuel@192.168.0.101>
2017-06-29 19:00:08 -0400

    Fixed req.files undefine issue.

--- CourseConnect/app/js/controllers/ctrl-userprofile.js
+++ CourseConnect/app/js/controllers/ctrl-userprofile.js
@@ -2,7 +2,7 @@
 
 var userProfileCtrls = angular.module('CtrlUserProfile', ['ngFileUpload']);
 
-userProfileCtrls.controller('UserProfileCtrl', ['$scope', '$http', 'fileUpload',
+userProfileCtrls.controller('UserProfileCtrl', ['$scope', '$http','fileUpload',
 	function ($scope, $http, fileUpload) {
 		$scope.profilePic = "img/defaultAvatar.png";
 
@@ -12,10 +12,24 @@ userProfileCtrls.controller('UserProfileCtrl', ['$scope', '$http', 'fileUpload',
 			if (result.data[0].UTorId != null) $scope.utorid = result.data[0].UTorId;
 			if (result.data[0].Description != null) $scope.description = result.data[0].Description;
 		})
-		
-		$scope.uploadFile = function () {
-			$http.post('/api/profpic-upload').then(function(res){
-				console.log(123);
-			})
+
+		$scope.uploadFile = function (file) {
+			var file = $scope.myFile;
+
+			console.log('file is ');
+			console.dir(file);
+
+			var uploadUrl = "/api/profpic-upload";
+			// fs.writeFile("/tmp/test", file, function(err) {
+			// 	if(err) {
+			// 		return console.log(err);
+			// 	}
+
+			// 	console.log("The file was saved!");
+			// }); 
+			fileUpload.uploadFileToUrl(file, uploadUrl);
+			// $http.post('/api/profpic-upload', {file}).then(function(res){
+			// 	console.log(123);
+			// })
 		};
 	}]);
--- CourseConnect/app/js/directives/directives.js
+++ CourseConnect/app/js/directives/directives.js
@@ -13,3 +13,19 @@ var directiveModule = angular.module('Directives', ['chart.js']); // Create or r
 directiveModule.directive('chartStackedBar', function (ChartJsFactory) {
     return new ChartJsFactory('StackedBar');
 });
+
+directiveModule.directive('fileModel', ['$parse', function ($parse) {
+    return {
+        restrict: 'A',
+        link: function (scope, element, attrs) {
+            var model = $parse(attrs.fileModel);
+            var modelSetter = model.assign;
+
+            element.bind('change', function () {
+                scope.$apply(function () {
+                    modelSetter(scope, element[0].files[0]);
+                });
+            });
+        }
+    };
+}]);
