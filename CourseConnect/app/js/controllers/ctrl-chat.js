ee5d78aa7ae2c7a786c57d1a2d3e00a76c954bc9
JONATHANG\Jonathan <tao.gao@mail.utoronto.ca>
2017-07-04 21:09:19 -0300

    [#147549935] Polished the view for better user experience

--- CourseConnect/app/js/controllers/ctrl-chat.js
+++ CourseConnect/app/js/controllers/ctrl-chat.js
@@ -85,6 +85,7 @@ chatCtrls.controller('ChatCtrl', ['$scope', '$http', 'fileUpload', '$cookies', '
                     storedFileloc = res.data;
                     console.log("The file has been stored at " +
                         storedFileloc[0].fileLocation);
+                    $scope.displayResource();
             });
         };
 
@@ -164,7 +165,7 @@ chatCtrls.controller('ChatCtrl', ['$scope', '$http', 'fileUpload', '$cookies', '
                     $scope.var_user_list = ChatService.getAllClassMates();
 
                     $scope.var_messages.push({"userId": 2, "profilePic": "img/profilePicDefault.jpg", "name": "bb", "message": "Hi, this is a test message from other user", "time": "2017-6-20 10:37:20"});
-
+                    $scope.displayResource();
                 };
 
                 $scope.init();
