4fffd4d3b4b6f3588ff107a2110e11cf39a9170e
Kevin Chen <guanyukevin.chen@gmail.com>
2017-07-08 17:39:33 -0400

    [#147149559] working code for reporting complaints

--- CourseConnect/app/js/controllers/ctrl-chat.js
+++ CourseConnect/app/js/controllers/ctrl-chat.js
@@ -296,8 +296,11 @@ chatCtrls.controller('ChatCtrl', ['$scope', '$http', 'fileUpload', '$cookies', '
         }
 
         $scope.submitReport = function (post) {
-            $http.post('/api/reportComplaint', {title: $scope.subject, quote: post, description: $scope.description}).then(function (res) {
+            console.log($scope);
+            $http.post('/api/reportComplaint', {title: $scope.subject[post.po_id], quote: post, description: $scope.description[post.po_id]}).then(function (res) {
+                if (res.data.reported == true) {
 
+                }
             })
 
         }
