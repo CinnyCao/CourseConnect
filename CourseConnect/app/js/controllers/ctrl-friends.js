b6d2fe0d1f7cdc6be9d92ac766c17d7c7ec568bd
Kevin Chen <guanyukevin.chen@gmail.com>
2017-07-18 00:41:11 -0400

    Displaying user's friend. UPCOMING: Unfriend and friend requests

--- CourseConnect/app/js/controllers/ctrl-friends.js
+++ CourseConnect/app/js/controllers/ctrl-friends.js
@@ -2,45 +2,12 @@
 
 var CtrlFriends = angular.module('CtrlFriends', []);
 
-CtrlFriends.directive('onErrorSrc', function($http) {
-    return {
-        link: function(scope, element, attrs) {
-            element.bind('error', function() {
-                if (attrs.src != attrs.onErrorSrc) {
-                    attrs.$set('src', attrs.onErrorSrc);
-                }
-            });
-        }
-    };
-});
-
-CtrlFriends.service('FriendsService', ['$http', function ($http) {
-	this.getFriends = function () {
-        return $http.get('/api/getFriends');
-    };
-
-	this.removeFriend = function (friendID){
-		return $http.post('/api/removeFriend', {
-			fid: friendID
-        });
-	};
-}]);
-
-
-CtrlFriends.controller('FriendsCtrl', ['$scope', '$http', '$cookies', 'FriendsService', function ($scope, $http, $cookies, FriendsService) {
+CtrlFriends.controller('FriendsCtrl', ['$scope', '$http', function ($scope, $http) {
     console.log('FriendsCtrl is running');
 
-	$scope.var_friends = [];
-
-	FriendsService.getFriends()
-		.then(function(data){
-			console.log(data);
-			if (data.data.isvalid){
-				$scope.var_friends = data.data.friends;
-			}
-		});
-
-	$scope.unfriend = function (id) {
-		FriendsService.removeFriend(id);
-	};
+    $http.get('/api/getFriends').success(function (res) {
+        $scope.var_friends = res;
+    }).error(function (res) {
+        console.log(res);
+    })
 }]);
--- CourseConnect/routes/api-friend.js
+++ CourseConnect/routes/api-friend.js
@@ -7,77 +7,15 @@ var db = require('./db_connection');  // db manager
 /**Get Friends */
 exports.getFriends = function (req, res) {
     if (req.session.userid) {
-        var friendQuery = "Call getFriendsByID('" + req.session.userid + "')";
+        var friendQuery = "SELECT User2, fileLocation, LastName, FirstName, DisplayName FROM UsersFriends INNER JOIN Users WHERE User1=" + req.session.userid + " AND User2=u_id;";
+        console.log(friendQuery);
         db.executeQuery(friendQuery, function (err, result) {
             if (err) {
                 console.error("ERROR: Failed to execute token query." + err);
-                res.status(200).send({ isvalid: false, friends: [] });
+                res.status(404).send("query failed to execute");
             }
             console.log(result);
-            if (result.length >= 1) {
-
-                console.log("SUCCESS: " + req.session.userid + " ID has friends." + result[0]);
-                res.status(200).send({ isvalid: true, friends: result[0] });
-            } else {
-                console.log("SUCCESS: " + req.session.userid + " ID is a lone wolf. DangerZone!");
-                res.status(200).send({ isvalid: true, friends: result[0] });
-            }
-        });
-    } else {
-        res.status(401).json({
-            error: "User not logged in"
-        });
-    }
-};
-
-exports.getFriendInfo = function (req, res) {
-    if (req.session.userid) {
-        var query = "SELECT fileLocation AS profilePic, " +
-            "CASE WHEN DisplayName IS NULL OR DisplayName = '' THEN CONCAT(FirstName, ' ', LastName) ELSE DisplayName END AS name " +
-            "FROM Users WHERE u_id IN ("+req.session.userid+", "+req.params.userid+")";
-        db.executeQuery(query, function (err, result) {
-            if (err) {
-                console.error(err);
-                res.status(500).json({
-                    error: "An unexpected error occurred when querying the database"
-                });
-            } else if (result.length) {
-                res.status(200).json(result);
-            } else {
-                res.sendStatus(404);
-            }
-        });
-    } else {
-        res.status(401).json({
-            error: "User not logged in"
-        });
-    }
-};
-
-exports.checkIsFriend = function (req, res) {
-    if (req.session.userid) {
-        var query = "SELECT * FROM Friends WHERE User1 IN ("+req.session.userid+", "+req.params.userid+") AND " +
-            "User2 IN ("+req.session.userid+", "+req.params.userid+") AND User1 != User2 AND hasAccepted = 1";
-        console.log(query);
-        db.executeQuery(query, function (err, result) {
-            if (err) {
-                console.error(err);
-                res.status(500).json({
-                    error: "An unexpected error occurred when querying the database"
-                });
-            } else if (result.length) {
-                res.status(200).json({
-                    isFriend: 1
-                });
-            } else {
-                res.status(200).json({
-                    isFriend: 0
-                });
-            }
-        });
-    } else {
-        res.status(401).json({
-            error: "User not logged in"
+            res.status(200).send(result);
         });
     }
 };
